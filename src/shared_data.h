42be1f72d850da55e66eafc0f8d2244b08f6273e
Mateo Andrioli <mandrioli@itba.edu.ar>
2026-03-19 15:05:36 -0300

    Funciones main y definicion inicial de estructuras

---
+++ src/shared_data.h
@@ -0,0 +1,24 @@
+#include <sys/wait.h>
+
+#define NAME_DIM 20
+#define CANT_PLAYERS 9
+
+/* Estructura para representar el estado del juego */
+typedef struct{
+    char playerName[NAME_DIM];
+    unsigned int playerScore;
+    unsigned int validMovements;
+    unsigned int invalidMovements;
+    unsigned int x;                     /* Posicion en el tablero */
+    unsigned int y;
+    pid_t playerPID;                    /* PID del proceso del jugador */
+    bool valid;                         /* Indica si el jugador tiene movimientos válidos */
+} Player;
+
+/* Estructura para representar el estado del tablero */
+typedef struct{
+    unsigned int width;                      /* Ancho del tablero */
+    unsigned int height;                     /* Alto del tablero */
+    Player players[CANT_PLAYERS];            /* Información de los jugadores */
+    bool gameOver;                      /* Indica si el juego ha terminado */
+} GameState;
---
+++ src/vista.c
@@ -0,0 +1,9 @@
+int main(int argc, char const *argv[]){
+    unsigned int heigth;
+    unsigned int width;
+
+    heigth=atoi(argv[1]);
+    width=atoi(argv[2]);
+    
+    return 0;
+}
